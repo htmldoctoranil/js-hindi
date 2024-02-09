6c7e9546f2d925cfcd15cfea571e8d2d2a18fdc3
htmldoctoranil <88539998+htmldoctoranil@users.noreply.github.com>
2024-02-09 12:11:53 +0000

    datatype added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1345;
+let acMail = "abc@gmail.com"
+let accountState
+
+acMail = "zyt@gmail.com"
+// accountId = 11 
+console.log(accountId);
+console.table([accountId, acMail, accountState
+])
